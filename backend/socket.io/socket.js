6b606da33102a9143b2ff247bb6668d89f088a4d
Neha <nehakash9519@gmail.com>
2024-10-21 19:09:53 +0530

    #

--- backend/socket.io/socket.js
+++ backend/socket.io/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:['https://chat-app-1-0-kq2a.onrender.com/login'],
+        origin:['http://localhost:5173'],
         methods:['GET','POST']
     }
 });
